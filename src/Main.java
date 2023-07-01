0177b7309ee2f6c354b3e39cd26787d716ed66f0
Elena Yalova <jalovalena9@gmail.com>
2023-07-01 18:49:51 +0300

    Изменен код в классе Main: Курсовой проект. Задание #1 по теме: Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -19,6 +19,36 @@ public class Main {
                 System.out.println("Путь указан верно");
                 number++;
                 System.out.println("Это файл номер " + number);
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader =
+                            new BufferedReader(fileReader);
+                    String line;
+                    int totalLines = 0;
+                    int maxLength = 0;
+                    int minLength = Integer.MAX_VALUE;
+                    while ((line = reader.readLine()) != null) {
+                        totalLines++;
+                        int length = line.length();
+                        if (length > maxLength) {
+                            maxLength = length;
+                        }
+                        if (length < minLength) {
+                            minLength = length;
+                        }
+                        if (line.length() > 1024) {
+                            throw new FileProcessingException("Строка в файле превышает 1024 символа: " + line);
+                        }
+                    }
+                    System.out.println("Общее количество строк в файле: " + totalLines);
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+                    reader.close();
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 continue;
             } else System.out.println("Файла не существует");
             continue;
